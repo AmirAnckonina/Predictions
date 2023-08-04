09d52ae8c84a2d8774348a46780b1573e62a6c45
amirkirshen <amirkirshenzwig@gmail.com>
2023-08-04 20:16:51 +0300

    insert some content to classes templates

--- Predictions/PredictionsEngine/src/simulatorWorld/entity/EntityList.java
+++ Predictions/PredictionsEngine/src/simulatorWorld/entity/EntityList.java
@@ -1,6 +1,12 @@
 package simulatorWorld.entity;
+import simulatorWorld.property.BaseProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntityList {
 
-    private List<Entity> entities = new Arraylist<>();
+    private List<Entity> entitiesList = new ArrayList<>();
+
+
 }
--- Predictions/PredictionsEngine/src/simulatorWorld/property/BaseProperty.java
+++ Predictions/PredictionsEngine/src/simulatorWorld/property/BaseProperty.java
@@ -2,4 +2,15 @@ package simulatorWorld.property;
 
 public class BaseProperty {
 
+    //should I add an enum for the options (int/float...)?
+    private int something;
+
+    public boolean setValue(Object value) {
+        return true;
+    }
+
+    public Object getValue(){
+        return this.something;
+    }
+
 }
