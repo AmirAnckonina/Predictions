d8775b58ac1acf25e8d1d7dc62a468136a22eea7
amirkirshen <amirkirshenzwig@gmail.com>
2023-09-05 10:01:32 +0300

    creating 2 subcomponents in the main component

--- Predictions/PredictionsUI/src/UI/impl/AppController.java
+++ Predictions/PredictionsUI/src/UI/impl/AppController.java
@@ -2,22 +2,31 @@ package UI.impl;
 import UI.dynamicbody.DynamicInfoController;
 import UI.staticheader.StaticHeaderController;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class AppController {
+    @FXML private ScrollPane headerComponent;
+    @FXML private StaticHeaderController headerComponentController;
+    @FXML private ScrollPane bodyComponent;
+    @FXML private DynamicInfoController bodyComponentController;
 
-    @FXML private StaticHeaderController headerController;
-    @FXML private DynamicInfoController bodyController;
-    @FXML private BorderPane main_layout_bp_id;
+    @FXML
+    public void initialize() {
+        if (headerComponentController != null && bodyComponentController != null) {
+            headerComponentController.setMainController(this);
+            bodyComponentController.setMainController(this);
+        }
+    }
 
     public void setHeaderComponentController(StaticHeaderController headerComponentController) {
-        this.headerController = headerComponentController;
+        this.headerComponentController = headerComponentController;
         headerComponentController.setMainController(this);
     }
 
     public void setBodyComponentController(DynamicInfoController bodyComponentController) {
-        this.bodyController = bodyComponentController;
+        this.bodyComponentController = bodyComponentController;
         bodyComponentController.setMainController(this);
     }
 }
