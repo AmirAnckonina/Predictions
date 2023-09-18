ba57fbd825fda6e029245ef8ddbed90b4a56e7b9
amirkirshen <amirkirshenzwig@gmail.com>
2023-09-19 00:46:51 +0300

    Update result subcomponent to support byProperty or byEntity. In addition, adding some logic to SimulationDocumentFacade

--- Predictions/PredictionsEngine/src/simulator/information/simulationDocument/api/SimulationDocumentFacade.java
+++ Predictions/PredictionsEngine/src/simulator/information/simulationDocument/api/SimulationDocumentFacade.java
@@ -1,4 +1,10 @@
 package simulator.information.simulationDocument.api;
 
+import java.util.Map;
+
 public interface SimulationDocumentFacade {
+
+    Map<String, Integer> getMappedEntitiesToNumOfEntities();
+    Map<String, Integer> getMappedPropertiesToNumOfEntitiesWithSameValues(String entityName);
+    Map<String,Integer> getAllEntityInstancesHasPropertyByPropertyName(String entityName, String propertyName);
 }
