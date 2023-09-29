41b16c09690a773d05a14840f73ab57f01dd7042
amirkirshen <amirkirshenzwig@gmail.com>
2023-09-29 15:19:45 +0300

    fix adminGui bug

--- Predictions/PredictionsAdministrator/src/ui/mainScene/MainController.java
+++ Predictions/PredictionsAdministrator/src/ui/mainScene/MainController.java
@@ -2,6 +2,7 @@ package ui.mainScene;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 import ui.allocations.AllocationsController;
@@ -16,25 +17,32 @@ public class MainController {
     @FXML private ManagementController managementTabController;
     @FXML private ExecutionsHistoryController executionHistoryTabController;
     @FXML private AllocationsController allocationsTabController;
+    @FXML private TabPane mainTabPane;
 
     @FXML
     public void initialize() {
-        ToggleGroup toggleGroup = new ToggleGroup();
+        try {
+            ToggleGroup toggleGroup = new ToggleGroup();
+        }catch (Exception e)
+        {
+            e.printStackTrace(System.out);
+        }
     }
 
     @FXML
     void allocationsTabClicked(ActionEvent event) {
-
+        System.out.println("allocationsTabClicked");
     }
 
     @FXML
     void executionsHistoryTabClicked(ActionEvent event) {
+        System.out.println("executionsHistoryTabClicked");
 
     }
 
     @FXML
-    void managementTabSelected(ActionEvent event) {
-
+    void managementTabClicked(ActionEvent event) {
+        System.out.println("executionsHistoryTabClicked");
     }
 
     public void setPrimaryStage(Stage primaryStage) {
