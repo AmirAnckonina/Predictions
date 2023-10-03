04eacd30103ae1a507eaba80c724bbc50182ca0d
amirkirshen <amirkirshenzwig@gmail.com>
2023-10-03 17:19:54 +0300

    complete Allocations.fxml screen (fxml only)

--- Predictions/PredictionsAdministrator/src/ui/tabs/allocations/AllocationsController.java
+++ Predictions/PredictionsAdministrator/src/ui/tabs/allocations/AllocationsController.java
@@ -1,16 +1,26 @@
 package ui.tabs.allocations;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class AllocationsController {
 
     @FXML
-    private Label label;
+    private ListView<Label> clientsLV;
 
     @FXML
-    public void initialize() {
-        System.out.println("a");
-    }
+    private ListView<Label> simulationsLV;
+
+    @FXML
+    private ListView<Label> allocationsLV;
+
+    @FXML
+    private TextField numOfAllocationTA;
+
+    @FXML
+    private Button allocateBtn;
+
 }
